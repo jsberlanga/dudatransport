0512f049ccd6d8cf18800485ea9967459384379d
unknown <jsberlanga@gmail.com>
2019-06-13 22:08:52 +0200

    404 updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
 import React from "react"
+import styled from "styled-components"
 
-import Layout from "../components/layout"
+import { PagesLayout } from "../components/layout"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
 import SEO from "../components/seo"
 
+import { styles } from "../utils"
+
+const Wrapper = styled.div`
+  margin: auto auto;
+  text-align: center;
+`
+
 const NotFoundPage = () => (
-  <Layout>
+  <PagesLayout
+    background={styles.colors.lightGray}
+    color={styles.colors.darkGray}
+  >
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Header navcolor={styles.colors.blue} logocolor="dark" />
+    <Wrapper style={{ gridArea: "main" }}>
+      <h1>Sorry, not found</h1>
+      <p>You just hit a route that doesn&#39;t exist...</p>
+    </Wrapper>
+    <Footer />
+  </PagesLayout>
 )
 
 export default NotFoundPage
