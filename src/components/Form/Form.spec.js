c866665fa9a460b0366c765df04b979898694bbe
Julio Soto <jsberlanga@gmail.com>
2019-12-22 23:21:11 +0100

    Feat/dev (#19)
    * feat(core): add user event library
    
    * feat(form): add alert message if fields are missing

--- src/components/Form/Form.spec.js
+++ src/components/Form/Form.spec.js
@@ -1,5 +1,6 @@
 import React from "react"
-import { render, fireEvent } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { render, fireEvent, wait } from "@testing-library/react"
 import { axe, toHaveNoViolations } from "jest-axe"
 import "@testing-library/jest-dom/extend-expect"
 import Form from "./Form"
@@ -9,7 +10,6 @@ expect.extend(toHaveNoViolations)
 const mockSuccessResponse = {}
 const mockJsonPromise = Promise.resolve(mockSuccessResponse) // 2
 const mockFetchPromise = Promise.resolve({
-  // 3
   json: () => mockJsonPromise,
 })
 global.fetch = jest.fn().mockImplementation(() => mockFetchPromise)
@@ -26,10 +26,27 @@ describe("Form", () => {
     const results = await axe(container)
     expect(results).toHaveNoViolations()
   })
-  it("submits the form", () => {
-    const { debug, getByText } = render(<Form {...props} />)
-    const button = getByText(/send/i)
-    fireEvent.click(button)
-    debug()
+  it("shows alert if fields are missing", () => {
+    const { getByRole } = render(<Form {...props} />)
+    userEvent.click(getByRole("button"))
+
+    expect(getByRole("alert")).toBeInTheDocument()
+  })
+  it("submits the form", async () => {
+    const { getByLabelText, queryByRole, getByRole } = render(
+      <Form {...props} />
+    )
+    userEvent.type(getByLabelText("name"), "test name")
+    expect(getByLabelText("name").value).toBe("test name")
+
+    userEvent.type(getByLabelText("email"), "test@email.com")
+    expect(getByLabelText("email").value).toBe("test@email.com")
+
+    userEvent.type(getByLabelText("message"), "test message")
+    expect(getByLabelText("message").value).toBe("test message")
+
+    userEvent.click(getByRole("button"))
+    expect(global.fetch).toHaveBeenCalledTimes(1)
+    expect(queryByRole("alert")).not.toBeInTheDocument()
   })
 })
