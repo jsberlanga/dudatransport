7fbe77ce31a6316686206115cca3526d70766e86
Julio Soto <julio.soto@schibsted.com>
2019-12-22 23:19:56 +0100

    feat(form): add alert message if fields are missing

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -13,6 +13,7 @@ const FormWrapper = styled.form`
   background-color: rgba(33, 33, 33, 0.7);
   padding: 1em 3em 2em;
   border-radius: 0.2rem;
+  position: relative;
 
   p {
     font-style: italic;
@@ -49,6 +50,24 @@ const FormWrapper = styled.form`
   textarea {
     height: 100%;
   }
+
+  .error {
+    display: block;
+    position: absolute;
+    top: -2.7rem;
+    left: 0;
+    right: 0;
+    text-align: center;
+    font-size: 1.3rem;
+    padding: 0.2rem;
+    background: #b00020;
+    color: ${styles.colors.mainLight};
+
+    @media (max-width: 768px) {
+      top: -2.5rem;
+      font-size: 1.2rem;
+    }
+  }
 `
 
 function encode(data) {
@@ -60,8 +79,14 @@ function encode(data) {
 class Form extends React.Component {
   state = {}
   handleSubmit = e => {
+    const { name, email, message } = this.state
+
     e.preventDefault()
     const form = e.target
+    if (!name || !email || !message) {
+      this.setState({ error: "Please fill out the form." })
+      return
+    }
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -77,51 +102,57 @@ class Form extends React.Component {
     this.setState({ [e.target.name]: e.target.value })
   }
   render() {
+    const { error } = this.state
     return (
-      <FormWrapper
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={this.handleSubmit}
-      >
-        <h2>{this.props.title}</h2>
-        <p>{this.props.subtitle}</p>
-        <input type="hidden" name="form-name" value="contact" />
-        <input
-          aria-label="name"
-          name="name"
-          placeholder="Name*"
-          type="text"
-          required
-          onChange={this.handleChange}
-        />
-        <input
-          aria-label="email"
-          name="email"
-          placeholder="Email*"
-          type="email"
-          required
-          onChange={this.handleChange}
-        />
-        <input
-          aria-label="phone"
-          name="phone"
-          placeholder="Phone Number"
-          type="text"
-          onChange={this.handleChange}
-        />
-        <textarea
-          aria-label="message"
-          name="message"
-          minLength="10"
-          maxLength="200"
-          placeholder="Message*"
-          onChange={this.handleChange}
-        />
-        <Button type="submit">Send</Button>
-      </FormWrapper>
+      <>
+        <FormWrapper
+          name="contact"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {error ? (
+            <p className="error" role="alert">
+              {error}
+            </p>
+          ) : null}
+          <h2>{this.props.title}</h2>
+          <p>{this.props.subtitle}</p>
+          <input type="hidden" name="form-name" value="contact" />
+          <input
+            aria-label="name"
+            name="name"
+            placeholder="Name*"
+            type="text"
+            onChange={this.handleChange}
+          />
+          <input
+            aria-label="email"
+            name="email"
+            placeholder="Email*"
+            type="email"
+            onChange={this.handleChange}
+          />
+          <input
+            aria-label="phone"
+            name="phone"
+            placeholder="Phone Number"
+            type="text"
+            onChange={this.handleChange}
+          />
+          <textarea
+            aria-label="message"
+            name="message"
+            minLength="10"
+            maxLength="200"
+            placeholder="Message*"
+            onChange={this.handleChange}
+          />
+          <Button type="submit">Send</Button>
+        </FormWrapper>
+      </>
     )
   }
 }
